ed0d6c45bb5c1e1df7a430f3e0b52149069a9894
JP-Repo-07 <jhonpaulriopay7@gmail.com>
2022-10-27 22:53:29 +0800

    DOM & Events: Currently taking Lessonn 49.1

---
+++ script.js
@@ -0,0 +1,13 @@
+//This is a comment
+
+/* This is a
+multi-line comment*/
+
+console.log("This is a string");
+
+var a = 10;
+
+var string = "Words";
+
+var isActive = true;
+var isNotActive = false;
